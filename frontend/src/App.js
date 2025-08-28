3dd0d4cad2543e57779a40d1657df5698d70b08b
karmartir <karmartir@gmail.com>
2025-08-28 15:40:26 -0400

    7. Custom bootstrap theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to ProShop!</h1>
+          <h1>Welcome to ProShop</h1>
         </Container>
       </main>
       <Footer/>
