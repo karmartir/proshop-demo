721620263e8d5c67b9ab0037bc9d144de6a6e51b
karmartir <karmartir@gmail.com>
2025-08-30 11:57:05 -0400

    Fix 2 add missed name in productModel

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,23 +1,14 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const reviewSchema = mongoose.Schema(
   {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
     user: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
-      ref: "User",
-    },
-    name: {
-      type: String,
-      required: true,
-    },
-    rating: {
-      type: Number,
-      required: true,
-    },
-    comment: {
-      type: String,
-      required: true,
+      ref: 'User',
     },
   },
   {
@@ -30,9 +21,21 @@ const productSchema = mongoose.Schema(
     user: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
-      ref: "User",
+      ref: 'User',
+    },
+    name: {
+      type: String,
+      required: true,
     },
     image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
       type: String,
       required: true,
     },
@@ -41,13 +44,15 @@ const productSchema = mongoose.Schema(
       required: true,
     },
     reviews: [reviewSchema],
-    brand: {
-      type: String,
+    rating: {
+      type: Number,
       required: true,
+      default: 0,
     },
-    category: {
-      type: String,
+    numReviews: {
+      type: Number,
       required: true,
+      default: 0,
     },
     price: {
       type: Number,
@@ -59,22 +64,12 @@ const productSchema = mongoose.Schema(
       required: true,
       default: 0,
     },
-    rating: {
-      type: Number,
-      required: true,
-      default: 0,
-    },
-    numReviews: {
-      type: Number,
-      required: true,
-      default: 0,
-    },
   },
   {
     timestamps: true,
   }
 );
 
-const Product = mongoose.model("Product", productSchema);
+const Product = mongoose.model('Product', productSchema);
 
 export default Product;
