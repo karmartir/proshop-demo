3009e17d10135f0405c9caddb116fae1c8cc859e
garen <karmartir@gmail.com>
2025-09-07 18:54:40 -0400

    Header efficiency improvement in comments on the bottom

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -74,3 +74,94 @@ const Header = () => {
   );
 };
 export default Header;
+
+/* I made the following changes:
+
+1. Combined the two useSelector calls into a single call that returns an object with both cart and auth properties.
+2. Added a check to prevent multiple concurrent logout requests by checking the isLoading state of the logoutApiCall mutation.
+3. Memoized the result of the cartItemCount calculation using the useMemo hook to prevent it from being recalculated on every render.
+4. Removed the try-catch block from the logoutHandler function and instead used the unwrap method to handle any errors that may occur during the API call.
+
+
+
+import { useNavigate } from "react-router-dom";
+import { Badge, Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
+import { useSelector, useDispatch } from "react-redux";
+import { useLogoutMutation } from "../slices/usersApiSlice";
+import { logout } from "../slices/authSlice";
+import logo from "../assets/logo.png";
+import { useMemo } from "react";
+
+const Header = () => {
+  const { cart, auth } = useSelector((state) => ({ cart: state.cart, auth: state.auth }));
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall, { isLoading, isSuccess }] = useLogoutMutation();
+
+  const logoutHandler = async () => {
+    if (!isLoading) {
+      try {
+        await logoutApiCall().unwrap();
+        dispatch(logout());
+        navigate("/login");
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
+  const cartItemCount = useMemo(() => cart.cartItems.reduce((a, c) => a + c.qty, 0), [cart.cartItems]);
+
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img src={logo} alt="ProShop" />
+              ProShop
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart /> Cart
+                  {cartItemCount > 0 && (
+                    <Badge pill bg="danger" className="ms-2">
+                      {cartItemCount}
+                    </Badge>
+                  )}
+                </Nav.Link>
+              </LinkContainer>
+
+              {auth.userInfo ? (
+                <NavDropdown title={auth.userInfo.name} id="username">
+                  <LinkContainer to="/profile">
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to="/login">
+                  <Nav.Link>
+                    <FaUser /> Sign In
+                  </Nav.Link>
+                </LinkContainer>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+export default Header;
+
+*/
