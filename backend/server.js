f6af686e57b019ec3878793e1c8cc0f334cc0ceb
garen <karmartir@gmail.com>
2025-09-08 12:33:40 -0400

    56. Order Routes & Controller

--- backend/server.js
+++ backend/server.js
@@ -6,27 +6,34 @@ import connectDB from './config/db.js';
 import { notFound, errorHandler  } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 
+// Set the port from environment variables or default to 5000
 const port = process.env.PORT || 5000;
 
 connectDB(); // Connect to MongoDB
 
 
-const app = express();
+const app = express(); // Initialize Express app
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies
 app.use(cookieParser()); // Middleware to parse cookies
 
+// Basic route to check if the API is running
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
+// Route Handlers
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
+// Error Handling Middleware
 app.use(notFound);
 app.use(errorHandler);
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
