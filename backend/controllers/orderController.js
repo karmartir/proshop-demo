a44ae27632943bea866582879e1daa5fd8b6d025
garen <karmartir@gmail.com>
2025-09-08 13:06:51 -0400

    57. Create & Get Orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,29 @@ import Order from "../models/orderModel.js";
 //@access  Private
 
 const addOrderItems = asyncHandler(async (req, res) => {
-  res.send('Add order items');
+  const {
+    orderItems, shippingAddress, paymentMethod, itemsPrice, taxPrice, shippingPrice, totalPrice,
+  } = req.body;
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error('No order items');
+
+  } else {
+    const order = new Order({
+      orderItems : orderItems.map((x) => ({ ...x, product: x._id, _id: undefined })),
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+
+    const createdOrder = await order.save();
+
+    res.status(201).json(createdOrder);
+  }
 });
 
 //@desc    Get logged in user orders
@@ -14,7 +36,8 @@ const addOrderItems = asyncHandler(async (req, res) => {
 //@access  Private
 
 const getMyOrders = asyncHandler(async (req, res) => {
-  res.send('get my orders');
+  const orders = await Order.find({ user: req.user._id });
+  res.status(200).json(orders);
 });
 
 //@desc Get order by ID
@@ -22,18 +45,17 @@ const getMyOrders = asyncHandler(async (req, res) => {
 //@access Private
 
 const getOrderById = asyncHandler(async (req, res) => {
-    res.send('Get order by ID');
-//   const order = await Order.findById(req.params.id).populate(
-//     'user',
-//     'name email'
-//   );
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  );
 
-//   if (order) {
-//     res.json(order);
-//   } else {
-//     res.status(404);
-//     throw new Error('Order not found');
-//   }
+  if (order) {
+    res.status(200).json(order);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
 });
 
 //@desc    Update order to paid
