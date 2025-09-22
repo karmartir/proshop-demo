0dc5d9844f3c80140c0b7dd00390f16d7f928849
garen <karmartir@gmail.com>
2025-09-22 19:53:32 -0400

    71. Edit Product

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -7,6 +7,7 @@ export const productsApiSlice = apiSlice.injectEndpoints({
       query: () => ({
         url: PRODUCTS_URL,
       }),
+      providesTags: ["Product"],
       keepUnusedDataFor: 5
     }),
     getProductDetails: builder.query({
@@ -22,13 +23,14 @@ export const productsApiSlice = apiSlice.injectEndpoints({
       }),
       invalidatesTags: ["Product"],
     }),
-    // updateProduct: builder.mutation({
-    //   query: (product) => ({
-    //     url: `${PRODUCTS_URL}/${product._id}`,
-    //     method: "PUT",
-    //     body: product,
-    //   }),
-    // }),
+    updateProduct: builder.mutation({
+      query: (data) => ({
+        url: `${PRODUCTS_URL}/${data.productId}`,
+        method: "PUT",
+        body: data,
+      }),
+      invalidatesTags: ["Product"],
+    }),
     // deleteProduct: builder.mutation({
     //   query: (ProductId) => ({
     //     url: `${PRODUCTS_URL}/${ProductId}`,
@@ -38,4 +40,4 @@ export const productsApiSlice = apiSlice.injectEndpoints({
   }),
 });
 
-export const { useGetProductsQuery, useGetProductDetailsQuery, useCreateProductMutation } = productsApiSlice;
+export const { useGetProductsQuery, useGetProductDetailsQuery, useCreateProductMutation, useUpdateProductMutation } = productsApiSlice;
