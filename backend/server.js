b14a74f0f1c09bb5d1f330fdfdb21f1461c9409d
garen <karmartir@gmail.com>
2025-09-25 22:50:21 -0400

    fix reload issue on deployment

--- backend/server.js
+++ backend/server.js
@@ -40,11 +40,24 @@ const __dirname = path.resolve(); // Get the current directory path
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 if (process.env.NODE_ENV === 'production') {
+  // Serve React build
   app.use(express.static(path.join(__dirname, '/frontend/build')));
-  // Serve index.html for all other routes
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+
+  // List of SPA routes
+  const spaRoutes = ['/', '/cart', '/login', '/profile', '/shipping', '/payment', '/placeorder'];
+
+  // Dynamic product route
+  app.get('/product/:id', (req, res) => {
+    res.sendFile(path.join(__dirname, '/frontend/build', 'index.html'));
   });
+
+  // Serve index.html for all SPA routes
+  spaRoutes.forEach(route => {
+    app.get(route, (req, res) => {
+      res.sendFile(path.join(__dirname, '/frontend/build', 'index.html'));
+    });
+  });
+
 } else {
   app.get('/', (req, res) => {
     res.send('API is running...');
